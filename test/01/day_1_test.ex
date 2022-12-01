c0b4acac2552403d94c5c7af3836d23eb856ac56
Peter van der Meulen <peter@hendrikpeter.net>
2022-12-01 16:36:38 +0100

    Small fixes

--- test/01/day_1_test.ex
+++ test/01/day_1_test.ex
@@ -1,16 +1,30 @@
 defmodule ElfCalories do
-  def elf_no_with_most_calories(list) do
+  @moduledoc """
+  This module gets the calories of different elves from a stringed list of snacks per elf
+  the divider between elves is a double newline ("\\n\\n").
+  the divider betwen snacks is a single newline ("\n").
+  """
+
+  @doc """
+  Return total calories of snacks for single elf with the most when providing a list
+  """
+  # @spec calories_of_elf_with_most_calories(string) :: number
+  def calories_of_elf_with_most_calories(list) do
     list
     |> split_per_elf()
-    |> map_to_elf_with_total_calories()
+    |> map_to_elves_with_total_calories()
     # Get single value with higest value through reducer
     |> Enum.reduce(&max/2)
   end
 
-  def total_calories_by_3_richest_elves(list) do
+  @doc """
+  Return total calories of snacks for 3 elves with the most when providing a list
+  """
+  # @spec total_calories_by_3_elves_with_most_calories(string) :: number
+  def total_calories_by_3_elves_with_most_calories(list) do
     list
     |> split_per_elf()
-    |> map_to_elf_with_total_calories()
+    |> map_to_elves_with_total_calories()
     |> Enum.sort()
     |> Enum.reverse()
     |> Enum.take(3)
@@ -21,7 +35,7 @@ defmodule ElfCalories do
     String.split(list, "\n\n")
   end
 
-  defp map_to_elf_with_total_calories(lists_per_elf) do
+  defp map_to_elves_with_total_calories(lists_per_elf) do
     lists_per_elf
     |> Enum.map(fn string_collection ->
       string_collection
@@ -39,6 +53,10 @@ end
 
 defmodule Day1Test do
   use ExUnit.Case
+
+  @moduledoc """
+  Assignment: https://adventofcode.com/2022/day/1
+  """
 
   def read_test do
     """
@@ -66,11 +84,11 @@ defmodule Day1Test do
   test "Puzzle 1: elf calories and elf number of elf with highest calories" do
     elf_list = read_test()
 
-    result = ElfCalories.elf_no_with_most_calories(elf_list)
+    result = ElfCalories.calories_of_elf_with_most_calories(elf_list)
     assert result == 24000
 
     real_elf_list = read_fixture()
-    real_result = ElfCalories.elf_no_with_most_calories(real_elf_list)
+    real_result = ElfCalories.calories_of_elf_with_most_calories(real_elf_list)
 
     IO.puts(" Puzzle 1 answer: << #{real_result} >>")
   end
@@ -78,11 +96,11 @@ defmodule Day1Test do
   test "Puzzle 2: total sum of 3 elves with most calories" do
     elf_list = read_test()
 
-    result = ElfCalories.total_calories_by_3_richest_elves(elf_list)
+    result = ElfCalories.total_calories_by_3_elves_with_most_calories(elf_list)
     assert result == 45000
 
     real_elf_list = read_fixture()
-    real_result = ElfCalories.total_calories_by_3_richest_elves(real_elf_list)
+    real_result = ElfCalories.total_calories_by_3_elves_with_most_calories(real_elf_list)
 
     IO.puts(" Puzzle 2 answer: << #{real_result} >>")
   end
